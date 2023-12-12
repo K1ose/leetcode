00939b02889c1fc59a6a3c5cb527d718c31da839
K1ose <klose@jk404.cn>
2023-12-12 14:22:48 +0800

    update

---
+++ daily_cpp/0496.next-greater-element-i.cpp
@@ -0,0 +1,79 @@
+/* 496. 下一个更大元素 I
+简单
+相关标签
+相关企业
+nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
+
+给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
+
+对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+
+返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+
+
+
+示例 1：
+
+输入：nums1 = [4,1,2], nums2 = [1,3,4,2].
+输出：[-1,3,-1]
+解释：nums1 中每个值的下一个更大元素如下所述：
+- 4 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+- 1 ，用加粗斜体标识，nums2 = [1,3,4,2]。下一个更大元素是 3 。
+- 2 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+示例 2：
+
+输入：nums1 = [2,4], nums2 = [1,2,3,4].
+输出：[3,-1]
+解释：nums1 中每个值的下一个更大元素如下所述：
+- 2 ，用加粗斜体标识，nums2 = [1,2,3,4]。下一个更大元素是 3 。
+- 4 ，用加粗斜体标识，nums2 = [1,2,3,4]。不存在下一个更大元素，所以答案是 -1 。
+
+
+提示：
+
+1 <= nums1.length <= nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 104
+nums1和nums2中所有整数 互不相同
+nums1 中的所有整数同样出现在 nums2 中
+
+
+进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？ */
+#include "../tools/program_head.h"
+#include "../tools/display.h"
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+        vector<int> ans;
+        int n1 = nums1.size(), n2 = nums2.size();
+        stack<int> stk;
+        unordered_map<int, int> um;
+        for (int i = n2 - 1; i >= 0; i--)
+        {
+            int num = nums2[i];
+            while (!stk.empty() && num >= stk.top())
+            {
+                stk.pop();
+            }
+            um[num] = stk.empty() ? -1 : stk.top();
+            stk.push(num);
+        }
+        for (int i = 0; i < n1; i++)
+        {
+            ans.push_back(um[nums1[i]]);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums1 = {4, 1, 2};
+    vector<int> nums2 = {1, 3, 4, 2};
+    Solution slt;
+    print_vec1d_arr(slt.nextGreaterElement(nums1, nums2));
+    return 0;
+}
